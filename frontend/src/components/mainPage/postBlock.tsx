ca1a9e3f9baecbbac49edac9826fa70a835c9f3b
yu-zhen <wlphg010688@gmail.com>
2021-09-14 21:08:41 +0800

    postBlock bug fixed

--- frontend/src/components/mainPage/postBlock.tsx
+++ frontend/src/components/mainPage/postBlock.tsx
@@ -19,17 +19,17 @@ const PostBlock = ({ post } : Props) => {
     const [ showComment, setShowComment ] = useState(false);
     const [ comment, setComment ] = useState("");
     const { user, setUser, shownPosts, setShownPosts } = useContext(WebContext);
-    const { isUpVoteBoxOn, setIsUpVoteBoxOn, isDownVoteBoxOn, setIsDownVoteBoxOn} = useContext(MainPageContext);
+    const { setIsUpVoteBoxOn, setIsDownVoteBoxOn } = useContext(MainPageContext);
     const shownVoters = 4;
 
-    const openVoteBox = (isUpvote: boolean) => {
-        if (isUpvote) {
-            setIsUpVoteBoxOn(true);
-            console.log('is up vote? ' + isUpVoteBoxOn);
-        } else {
-            setIsDownVoteBoxOn(true);
-            console.log('is down vote? ' + isDownVoteBoxOn);
-        }    
+    const openUpvote = (event: any) => {
+        event.stopPropagation();
+        setIsUpVoteBoxOn(true);
+    }
+
+    const openDownvote = (event: any) => {
+        event.stopPropagation();
+        setIsDownVoteBoxOn(true);
     }
 
     const upvote = async () => {
@@ -101,9 +101,8 @@ const PostBlock = ({ post } : Props) => {
                 <div className="epk-icon"><Jdenticon size="24" value={post.epoch_key} /></div>
                 <div className="rep">{post.reputation}</div>
                 <div className="epk">{post.epoch_key}</div>
-                {/* <div className="vote" onClick={() => openVoteBox(true)}><img src="/images/upvote.png"></img>{post.upvote}</div> */}
-                <div className="vote" onClick={upvote}><img src="/images/upvote.png"></img>{post.upvote}</div>
-                <div className="vote" onClick={() => openVoteBox(false)}><img src="/images/downvote.png"></img>{post.downvote}</div>
+                <div className="vote" onClick={openUpvote}><img src="/images/upvote.png"></img>{post.upvote}</div>
+                <div className="vote" onClick={openDownvote}><img src="/images/downvote.png"></img>{post.downvote}</div>
             </div>
             <div className="post-block-main">
                 <div className="post-block-info">
